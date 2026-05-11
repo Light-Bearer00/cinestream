17fc5812277d961ae6452e7ec5a0b2bcc8a03de8
72-skeleton <hackworld087@gmail.com>
2026-05-11 04:06:32 +0300

    fix 404 on refresh - remove static export

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
@@ -19,6 +18,4 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
   },
 };
-
 module.exports = nextConfig;
-
