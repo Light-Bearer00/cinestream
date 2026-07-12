4ce697493b647be7f2fa569e969cc1c45d6d535d
72-skeleton <hackworld087@gmail.com>
2026-07-13 02:40:05 +0300

    update Railway URL to royalqueen2009

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -5,7 +5,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'https://royalqueen.up.railway.app';
+const API_URL = 'https://royalqueen2009.up.railway.app';
 
 // Create axios instance
 const api = axios.create({
